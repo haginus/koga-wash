1350d7ef56a97bb53120e4e1e29c144104cae558
Haginus <hagiandrei.ah@gmail.com>
2023-01-31 18:49:44 +0200

    list mail templates

--- src/dev/dev.controller.ts
+++ src/dev/dev.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Param } from '@nestjs/common';
 import { Dev } from 'src/auth/decorators/dev.decorator';
 import { Public } from 'src/auth/decorators/public.decorator';
 import { MailService } from 'src/mail/mail.service';
+import { mailContexts } from 'src/mail/mock';
 
 @Controller('dev')
 @Dev()
@@ -10,8 +11,21 @@ export class DevController {
 
   constructor(private mailService: MailService) {}
 
+  @Get('mail')
+  async listMailTemplates() {
+    const templates = Object.keys(mailContexts).map(templateName => `
+      <li><a href="${templateName}">${templateName}</a></li>
+    `);
+    return `
+      <h1>Mail templates</h1>
+      <ul>
+        ${templates.join('')}
+      </ul>
+    `;
+  }
+
   @Get('mail/:template')
-  async test(@Param('template') template: string) {
+  async renderTemplate(@Param('template') template: string) {
     return this.mailService.getMockMail(template);
   }
 }
